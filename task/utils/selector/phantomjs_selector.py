93e4d263141c48d6a2f4456993616ed00df7e2b0
huxuesen <huxuesen@outlook.com>
2022-07-11 15:21:15 +0800

    fix

--- task/utils/selector/phantomjs_selector.py
+++ task/utils/selector/phantomjs_selector.py
@@ -9,8 +9,47 @@ class PhantomJSSelector(FatherSelector):
         self.debug = debug
 
     def get_html(self, url, headers):
+        browser_args = [
+            '--window-size=1300,570',
+            '--window-position=000,000',
+            '--disable-dev-shm-usage',
+            '--no-sandbox',
+            '--disable-web-security',
+            '--disable-features=site-per-process',
+            '--disable-setuid-sandbox',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu',
+            '--use-gl=egl',
+            '--disable-blink-features=AutomationControlled',
+            '--disable-background-networking',
+            '--enable-features=NetworkService,NetworkServiceInProcess',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-breakpad',
+            '--disable-client-side-phishing-detection',
+            '--disable-component-extensions-with-background-pages',
+            '--disable-default-apps',
+            '--disable-extensions',
+            '--disable-features=Translate',
+            '--disable-hang-monitor',
+            '--disable-ipc-flooding-protection',
+            '--disable-popup-blocking',
+            '--disable-prompt-on-repost',
+            '--disable-renderer-backgrounding',
+            '--disable-sync',
+            '--force-color-profile=srgb',
+            '--metrics-recording-only',
+            '--enable-automation',
+            '--password-store=basic',
+            '--use-mock-keychain',
+            '--hide-scrollbars',
+            '--mute-audio'
+        ]
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=False, args=browser_args)
             page = browser.new_page()
             #page.add_init_script(js)1
             if headers:
